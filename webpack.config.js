23d228ad3874085ca19efa41650d477ba3c4a358
Areola Kelvin Mayokun <areola.kelvin@stu.cu.edu.ng>
2023-09-08 15:05:42 +0100

    Building

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/script.js', // Adjust the entry point to your script.js
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'), // Serve files from the 'public' directory
+    contentBase: path.resolve(__dirname, 'public'), // Serve files from the 'public' folder
     compress: true,
     port: 2004, // Choose a port for the server
   },
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src'), // Specify the 'src' folder
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
@@ -34,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html', // HTML template in the root directory
+      template: './src/index.html',
     }),
   ],
 };
